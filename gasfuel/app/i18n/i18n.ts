93c9726b1ed25783dd8d8ad464f96aa23c47ada3
Whiso <luis.rodas@digitalvaultstudio.com>
2021-02-01 12:47:58 -0800

    removed japan (ja) language trasnlate

--- gasfuel/app/i18n/i18n.ts
+++ gasfuel/app/i18n/i18n.ts
@@ -2,10 +2,9 @@ import * as Localization from "expo-localization"
 import i18n from "i18n-js"
 
 const en = require("./en")
-const ja = require("./ja")
 const sp = require("./sp")
 
 i18n.fallbacks = true
-i18n.translations = { en, ja, sp }
+i18n.translations = { en, sp }
 
-i18n.locale = Localization.locale || "sp"
+i18n.locale = Localization.locale || "en"
