9030e0e6f734b7c10dba8e365eafc108e833fa2d
Whiso <luis.rodas@digitalvaultstudio.com>
2021-01-31 17:16:53 -0800

    added spanish translate strings

--- gasfuel/app/i18n/i18n.ts
+++ gasfuel/app/i18n/i18n.ts
@@ -3,8 +3,9 @@ import i18n from "i18n-js"
 
 const en = require("./en")
 const ja = require("./ja")
+const sp = require("./sp")
 
 i18n.fallbacks = true
-i18n.translations = { en, ja }
+i18n.translations = { en, ja, sp }
 
-i18n.locale = Localization.locale || "en"
+i18n.locale = Localization.locale || "sp"
